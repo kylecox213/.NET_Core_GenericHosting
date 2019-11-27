ee19ddeba7c00313c4cc961ae3131ce8012169e1
Alex Cox <Acox@MAPCOM.LOCAL>
2019-11-27 15:08:29 -0500

    added updated files created in VS2019; complete starter appconfig settings json files

---
+++ GenericHostSample/Program.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.Extensions.Hosting;
+using System;
+using System.Threading.Tasks;
+
+namespace GenericHostSample
+{
+    class Program
+    {
+        public static async Task Main(string[] args)
+        {
+            var host = new HostBuilder()
+                .ConfigureHostConfiguration(configHost =>
+                {
+                    configHost.SetBasePath(Directory.GetCurrentDirectory());
+                })
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ GenericHostSample/TimedHostedService.cs
@@ -0,0 +1,31 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+
+namespace GenericHostSample
+{
+    #region snippet1
+
+    internal class TimedHostedService : IHostedService, IDisposable
+    {
+        private readonly ILogger _logger;
+        private Timer _timer;
+
+        public TimedHostedService(ILogger<TimedHostedService> logger)
+        {
+            _logger = logger;
+        }
+        public Task StartAsync(CancellationToken cancellationToken)
+        {
+            public Task StartAsync(CancellationToken cancellationToken);
+        }
+
+        public Task StopAsync(CancellationToken cancellationToken)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        private readonly ILogger 
+    }
+}
