8430bd141709a597dc1a1ba3f73e7724f95d9926
Alex Cox <Acox@MAPCOM.LOCAL>
2019-12-02 11:13:19 -0500

    file restoration because vital source code was not saved prior to last commit

--- GenericHostSample/Program.cs
+++ GenericHostSample/Program.cs
@@ -1,10 +1,13 @@
-﻿using Microsoft.Extensions.Hosting;
-using System;
+﻿using System.IO;
 using System.Threading.Tasks;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
 
 namespace GenericHostSample
 {
-    class Program
+    public class Program
     {
         public static async Task Main(string[] args)
         {
@@ -12,8 +15,33 @@ namespace GenericHostSample
                 .ConfigureHostConfiguration(configHost =>
                 {
                     configHost.SetBasePath(Directory.GetCurrentDirectory());
+                    configHost.AddJsonFile("hostsettings.json", optional: true);
+                    configHost.AddEnvironmentVariables(prefix: "PREFIX_");
+                    configHost.AddCommandLine(args);
                 })
-            Console.WriteLine("Hello World!");
+                .ConfigureAppConfiguration((hostContext, configApp) =>
+                {
+                    configApp.AddJsonFile("appsettings.json", optional: true);
+                    configApp.AddJsonFile(
+                        $"appsettings.{hostContext.HostingEnvironment.EnvironmentName}.json", 
+                        optional: true);
+                    configApp.AddEnvironmentVariables(prefix: "PREFIX_");
+                    configApp.AddCommandLine(args);
+                })
+                .ConfigureServices((hostContext, services) =>
+                {
+                    services.AddHostedService<LifetimeEventsHostedService>();
+                    services.AddHostedService<TimedHostedService>();
+                })
+                .ConfigureLogging((hostContext, configLogging) =>
+                {
+                    configLogging.AddConsole();
+                    configLogging.AddDebug();
+                })
+                .UseConsoleLifetime()
+                .Build();
+
+            await host.RunAsync();
         }
     }
 }
--- GenericHostSample/TimedHostedService.cs
+++ GenericHostSample/TimedHostedService.cs
@@ -1,4 +1,6 @@
-﻿using System.Threading;
+﻿  
+using System;
+using System.Threading;
 using System.Threading.Tasks;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
@@ -6,7 +8,6 @@ using Microsoft.Extensions.Logging;
 namespace GenericHostSample
 {
     #region snippet1
-
     internal class TimedHostedService : IHostedService, IDisposable
     {
         private readonly ILogger _logger;
@@ -16,16 +17,35 @@ namespace GenericHostSample
         {
             _logger = logger;
         }
+
         public Task StartAsync(CancellationToken cancellationToken)
         {
-            public Task StartAsync(CancellationToken cancellationToken);
+            _logger.LogInformation("Timed Background Service is starting.");
+
+            _timer = new Timer(DoWork, null, TimeSpan.Zero, 
+                TimeSpan.FromSeconds(5));
+
+            return Task.CompletedTask;
+        }
+
+        private void DoWork(object state)
+        {
+            _logger.LogInformation("Timed Background Service is working.");
         }
 
         public Task StopAsync(CancellationToken cancellationToken)
         {
-            throw new System.NotImplementedException();
+            _logger.LogInformation("Timed Background Service is stopping.");
+
+            _timer?.Change(Timeout.Infinite, 0);
+
+            return Task.CompletedTask;
         }
 
-        private readonly ILogger 
+        public void Dispose()
+        {
+            _timer?.Dispose();
+        }
     }
+    #endregion
 }
